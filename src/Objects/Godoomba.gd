07bbba47da3f5a072502f9a3b474adad20464cb4
Kyle Reese <reesekyle1998@gmail.com>
2022-06-09 20:43:14 -0400

    Godoomba moves correctly now

--- src/Objects/Godoomba.gd
+++ src/Objects/Godoomba.gd
@@ -3,17 +3,33 @@ extends KinematicBody2D
 
 const GRAVITY = 1000
 const HIT_HURT_ANGLE = 0.8
+const SPEED := 100
 
-var velocity := Vector2(-100, 0)
+var velocity := Vector2(-SPEED, 0)
+var direction := -1
 
 
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
-	var checkLeft = not $GodoombaWalkingLeft.is_colliding()
-	var checkRight = not $GodoombaWalkingRight.is_colliding()
+	# print_debug("Velocity = %.2f " % velocity.x)
+	var checkLeft = not $GodoombaWalkingLeft.is_colliding() and $GodoombaWalkingLeft.is_enabled()
+	var checkRight = not $GodoombaWalkingRight.is_colliding() and $GodoombaWalkingRight.is_enabled()
 	if ((checkLeft or checkRight) and is_on_floor()) or is_on_wall():
-		velocity.x *= -1
+		direction *= -1
+		velocity.x = direction * SPEED
+		if(checkLeft):
+			# print_debug("My left foot is over an edge and I'm turning around")
+			$GodoombaWalkingLeft.enabled = false
+			$GodoombaWalkingRight.enabled = true
+		elif(checkRight):
+			# print_debug("My right foot is over an edge and I'm turning around")
+			$GodoombaWalkingRight.enabled = false
+			$GodoombaWalkingLeft.enabled = true
+		elif(is_on_wall()):
+			# print_debug("I hit a wall and am turning around")
+			$GodoombaWalkingRight.enabled = true
+			$GodoombaWalkingLeft.enabled = true
 
 
 func _on_GodoombaHitHurtBox_body_entered(body: Node) -> void:
