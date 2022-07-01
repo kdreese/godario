16fb57ba54f67df6206095340b9f29b51ce08cde
Kyle Reese <reesekyle1998@gmail.com>
2022-06-30 21:26:14 -0400

    Kill player under death line and smooth out camera

--- src/Game/GameCamera.gd
+++ src/Game/GameCamera.gd
@@ -7,3 +7,6 @@ var player: KinematicBody2D
 func _process(_delta: float) -> void:
 	if player:
 		position = player.position
+		var target_y := -get_viewport_rect().size.y / 2
+		if position.y > target_y:
+			position.y = target_y
